49d90fcd8ae8a21cb33bcc42df35596f1571287d
Sam Davey <samkdavey@gmail.com>
2022-06-01 16:19:57 +0700

    Pagination functional

--- web-developer-test/src/App.js
+++ web-developer-test/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import PokemonList from './PokemonList'
+import Pagination from './Pagination'
 import axios from 'axios'
 //This uses the axios library which allows async requests to the Poke API
 
@@ -11,16 +12,44 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get(currentPageUrl).then(res => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
       setLoading(false)
       setnextPageUrl(res.data.next)
       setprevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
-    });
+    })
+    //Cancel a request everytime we make a new one, incase an old request finishes after a new one
+    return () => { cancel()}
   }, [currentPageUrl])
 
+
+  //Go to next page
+  function gotoNextPage(){
+    setCurrentPageUrl(nextPageUrl)
+  }
+  
+  //Go to previous page
+  function gotoPreviousPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  //Generic loading message to display to the user
+  if(loading) return "Loading..."
+
   return (
+    <>
+    <h1>Pokemon List</h1>
+    <input type="text" placeholder="Search Pokemon..."></input>
     <PokemonList pokemon={pokemon}/>
+    <Pagination 
+    gotoNextPage = {nextPageUrl ? gotoNextPage : null}
+    gotoPrevPage = {prevPageUrl ? gotoPreviousPage : null}
+    />
+    </>
   );
 }
 
