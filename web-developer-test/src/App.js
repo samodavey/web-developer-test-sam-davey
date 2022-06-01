da76a6a296b8b0e20575adeed4e621c63cc2f461
Sam Davey <samkdavey@gmail.com>
2022-06-01 15:32:27 +0700

    Generic list of pokemon rendered

--- web-developer-test/src/App.js
+++ web-developer-test/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import PokemonList from './PokemonList'
+import axios from 'axios'
+//This uses the axios library which allows async requests to the Poke API
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setnextPageUrl] = useState()
+  const [prevPageUrl, setprevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setnextPageUrl(res.data.next)
+      setprevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    });
+  }, [currentPageUrl])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PokemonList pokemon={pokemon}/>
   );
 }
 
